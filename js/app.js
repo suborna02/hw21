197509c8dc90ce8ef3afb6ba06543bea993b25ee
suborna02 <118161321+suborna02@users.noreply.github.com>
2023-02-13 13:37:52 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,75 @@
+// $(document).ready(function(){
+   // $('#show').on('click', function(){
+      // $('#title').show()
+
+
+
+      $(document).ready(function(){
+
+      
+      // $('#haider').on('click', function(){
+         // $('#title').slideToggle('slow',function(){
+            // alert()
+         // })
+      // })
+
+
+
+      // $('#toggle').on('click', function(){
+         // $('#search_bar').toggleAddClass('.active')
+      // })
+
+
+      // document.getElementById('toggle').addEventListener('click', function(){
+         // document.getAnimations('search_bar').classList.toggle('active')
+      // })
+
+
+       $('#btn').on('click', function(){
+          $('#sidebar').toggleClass('active');
+
+       })
+
+
+      $('#toggle').on('click', function(){
+         $('#search_bar').toggleClass('active');
+      })
+
+      $(window).on('load', function(){
+          $('.preloader_container').slideUp(2500);
+       })
+
+      
+
+
+      
+
+
+   })
+
+
+   // document.getElementById('toggle').addEventListener('click', function(){
+      // document.getElementById('search_bar').classList.toggle('active')
+   // })
+
+
+   // document.getElementById('menu_btn').addEventListener('click', function(){
+      // document.getElementById('sidebar').classList.toggle('active')
+   // })
+
+
+
+
+      
+   
+
+
+
+
+
+
+
+
+ 
+
+
